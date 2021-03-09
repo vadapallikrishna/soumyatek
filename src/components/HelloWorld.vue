28922451f1610474b23c2f3505c0da07a7033ef8
Krishna Satya <vadapallikrishna@gmail.com>
2021-03-09 13:36:50 +0530

    info

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="hello">
     <h1>Hello World from Soumya Technologies</h1>
+    <h1>WhatsApp at +918374265403</h1>
+    <div class="services">
+      <h3>Basic Services</h3>
+      <div class="inline">
+        <h2>Wordpress</h2>
+        <h2>Graphic Design</h2>
+        <h2>Web Design</h2>
+        <h2>Cpanel</h2>
+        <h2>React</h2>
+        <h2>Vue</h2>
+      </div>
+    </div>
+    <div class="services">
+      <h3>Cloud Services</h3>
+      <div class="inline">
+        <h2>Software Installation</h2>
+        <h2>Saas</h2>
+        <h2>Database Administration</h2>
+        <h2>Api Testing</h2>
+      </div>
+    </div>
+    <div class="services">
+      <h3>Mobile App Development</h3>
+      <div class="inline">
+        <h2>Android</h2>
+        <h2>React Native</h2>
+        <h2>Flutter</h2>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -11,4 +40,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .inline {
+    display: inline-flex;
+  }
 </style>
