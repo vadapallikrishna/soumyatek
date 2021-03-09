d3a6dce5d7368c4294564f4d721e7bb66a9f3a35
Krishna Satya <vadapallikrishna@gmail.com>
2021-03-09 13:09:24 +0530

    :

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Hello World from Soumya</h1>
+    <h1>Hello World from Soumya Technologies</h1>
 </template>
 
 <script>
@@ -11,18 +11,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
