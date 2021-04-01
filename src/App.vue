a24a4c20c535bfd089373b312ff9a8b805d9f11d
Krishna Satya <vadapallikrishna@gmail.com>
2021-04-01 16:51:38 +0530

    small chat

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
    <div class="inline">
     <router-view/>
+    <script src="https://embed.small.chat/T01SYLLBU5BC01TEFRQ1CH.js" async></script>
       <p> &copy; Soumya Technologies </p>
   </div>
 
